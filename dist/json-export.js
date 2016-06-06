177379d54ccf94ed7970ac6eaace27bf717054b7
Eduardo Diaz <eduardiazf@gmail.com>
2016-06-06 10:34:09 -0400

    replace filesaver by window open

--- dist/json-export.js
+++ dist/json-export.js
@@ -4,9 +4,7 @@ var _xtend = require('xtend');
 
 var _xtend2 = _interopRequireDefault(_xtend);
 
-var _filesaver = require('filesaver.js');
-
-var _filesaver2 = _interopRequireDefault(_filesaver);
+var _jsBase = require('js-base64');
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
@@ -17,16 +15,21 @@ var defaults = {
 };
 
 var tpl = '<html xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:x="urn:schemas-microsoft-com:office:{{type}}" xmlns="http://www.w3.org/TR/REC-html40">';
-tpl += '<head><!--[if gte mso 9]><xml><x:ExcelWorkbook><x:ExcelWorksheets><x:ExcelWorksheet><x:Name>{{name}}</x:Name>';
+tpl += '<head><!--[if gte mso 9]><xml><x:ExcelWorkbook><x:ExcelWorksheets><x:ExcelWorksheet><x:Name>download</x:Name>';
 tpl += '<x:WorksheetOptions><x:DisplayGridlines/></x:WorksheetOptions></x:ExcelWorksheet></x:ExcelWorksheets></x:ExcelWorkbook>';
 tpl += '</xml><![endif]--></head><body><table>{{table-data}}</table></body></html>';
+
+var join = function join(arr) {
+  return arr.join('');
+};
 
 var jsonExport = function jsonExport(args) {
   var opts = (0, _xtend2.default)(args, defaults);
 
-  var thead = '<thead><tr>' + opts.header.map(function (header) {
+  var thead = '<thead><tr>' + join(opts.header.map(function (header) {
     return '<th>' + header + '</th>';
-  }).join('') + '</tr></thead>';
+  })) + '</tr></thead>';
+
   var tbody = '<tbody>' + opts.data.map(function (dt) {
     return '<tr>' + opts.keys.map(function (k) {
       return '<td>' + dt[k] + '</td>';
@@ -34,10 +37,9 @@ var jsonExport = function jsonExport(args) {
   }).join('') + '</tbody>';
 
   var data = tpl.replace('{{table-data}}', '' + thead + tbody).replace('{{type}}', opts.type);
+  var base64Data = _jsBase.Base64.encode(data);
 
-  _filesaver2.default.saveAs(new Blob([data], {
-    type: 'application/vnd.ms-excel'
-  }), opts.filename + '.' + opts.ext);
+  window.open('data:application/vnd.ms-' + opts.type + ';filename=' + opts.filename + '.doc;base64,' + base64Data);
 };
 
 module.exports = jsonExport;
--- json-export.js
+++ json-export.js
@@ -1,5 +1,5 @@
 import xtend from 'xtend';
-import FileSaver from 'filesaver.js';
+import { Base64 } from 'js-base64';
 
 
 const defaults = {
@@ -9,21 +9,23 @@ const defaults = {
 };
 
 let tpl = '<html xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:x="urn:schemas-microsoft-com:office:{{type}}" xmlns="http://www.w3.org/TR/REC-html40">'
-  tpl += '<head><!--[if gte mso 9]><xml><x:ExcelWorkbook><x:ExcelWorksheets><x:ExcelWorksheet><x:Name>{{name}}</x:Name>'
+  tpl += '<head><!--[if gte mso 9]><xml><x:ExcelWorkbook><x:ExcelWorksheets><x:ExcelWorksheet><x:Name>download</x:Name>'
   tpl += '<x:WorksheetOptions><x:DisplayGridlines/></x:WorksheetOptions></x:ExcelWorksheet></x:ExcelWorksheets></x:ExcelWorkbook>'
   tpl += '</xml><![endif]--></head><body><table>{{table-data}}</table></body></html>';
+
+const join = (arr) => (arr.join(''));
 
 const jsonExport = (args) => {
   const opts = xtend(args, defaults);
 
   const thead = `<thead><tr>${opts.header.map((header) => (`<th>${header}</th>`)).join('')}</tr></thead>`;
+
   const tbody = `<tbody>${opts.data.map((dt) => (`<tr>${opts.keys.map((k) => (`<td>${dt[k]}</td>`)).join('')}</tr>`)).join('')}</tbody>`;
 
   const data = tpl.replace('{{table-data}}', `${thead}${tbody}`).replace('{{type}}', opts.type);
+	const base64Data = Base64.encode(data);
 
-  FileSaver.saveAs(new Blob([data], {
-    type: 'application/vnd.ms-excel',
-  }), `${opts.filename}.${opts.ext}`);
+	window.open(`data:application/vnd.ms-${opts.type};filename=${opts.filename}.doc;base64,${base64Data}`);
 }
 
 module.exports = jsonExport;
